7e528869ca8159d61f0f6cae5f17278c81b6a7fb
Bhaikko <siddhartha.pawar17@gmail.com>
2020-08-02 23:21:03 +0530

    File mappings added

---
+++ file_mapping.js
@@ -0,0 +1,31 @@
+// This fil contains Mapping of Different breeds predicted by Python model and Json file corresponding to it
+
+const FILE_MAPPINGS = {
+//  "python_prediction": "file_name"
+    "chihuahua": "chihuahua.json",
+    "shih-tzu": "shih-tzu.json",
+    "beagle": "beagle",
+    "yorkshire_terrier": "yorkshire-terrier",
+    "miniature_schnauzer": "miniature-schnauzer",
+    "golder_retriever": "golden-retriever",
+    "labrador_retriever": "labrador-retriever",
+    "german_short-haired_pointer" :"german-shorthaired-pointer",
+    "brittany_spaniel" :"brittany",
+    "english_springer" :"english-springer-spaniel",
+    "cocker_spaniel" :"cocker-spaniel",
+    "shetland_sheepdog" :"shetland-sheepdog",
+    "rottweiler" :"rottweiler",
+    "german_shepherd" :"german-shepherd-dog",
+    "doberman" :"doberman-pinscher",
+    "bernese_mountain_dog" :"bernese-mountain-dog",
+    "boxer" :"boxer",
+    "bull_mastiff" :"bullmastiff",
+    "french_bulldog" :"french-bulldog",
+    "great_dane" :"great-dane",
+    "eskimo_dog" :"american-eskimo-dog",
+    "siberian_husky" :"siberian-husky",
+    "pug" :"pug",
+    "pomeranian" :"pomeranian",
+    "pembroke" :"pembroke-welsh-corgi",
+    "standard_poodle" :"poodle"
+}
